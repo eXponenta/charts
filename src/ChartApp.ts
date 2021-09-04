d542f8c11f8c54993b09095c688ebe900e3ca5d5
Konstantin Timoshenko <rondo.devil@gmail.com>
2021-09-04 15:15:02 +0300

    feat: Base charts classes

---
+++ src/ChartApp.ts
@@ -0,0 +1,78 @@
+import { Renderer, BatchRenderer } from "@pixi/core";
+import { InteractionManager } from "@pixi/interaction";
+import { Container } from "@pixi/display";
+import {Chart, CHART_EVENTS} from "./Chart";
+
+Renderer.registerPlugin('batch', BatchRenderer);
+Renderer.registerPlugin('interaction', InteractionManager);
+
+export class ChartApp {
+    readonly renderer: Renderer;
+    readonly stage: Container;
+
+    constructor(canvasOrId: HTMLCanvasElement | string) {
+        const view = canvasOrId instanceof HTMLCanvasElement
+                ? canvasOrId
+                : document.querySelector<HTMLCanvasElement>(canvasOrId);
+
+        const scale = window.devicePixelRatio;
+        this.renderer = new Renderer({
+            view,
+            width: view.clientWidth * scale,
+            height: view.clientHeight * scale,
+        });
+
+        this.stage = new Container();
+
+        this.draw = this.draw.bind(this);
+        this.onChartUpdate = this.onChartUpdate.bind(this);
+        this.removeChart = this.removeChart.bind(this);
+    }
+
+    private onChartUpdate(chart: Chart) {
+        this.draw();
+    }
+
+    public addChart(chart: Chart, name = 'chart_' + this.stage.children.length): Chart {
+        if (this.stage.children.includes(chart)) {
+            return  chart;
+        }
+
+        chart.name = name;
+
+        this._bindEvents(chart);
+
+        this.stage.addChild(chart);
+
+        this.draw();
+
+        return chart;
+    }
+
+    public removeChart(name: string): Chart {
+        const chart: Chart = this.stage.children.find( (e: any) => e.name === name) as Chart;
+
+        if (chart) {
+            this._unbindEvents(chart);
+            this.stage.removeChild(chart)
+            this.draw();
+        }
+
+
+        return chart;
+    }
+
+    private _unbindEvents (chart: Chart): void {
+        chart.removeListener(CHART_EVENTS.UPDATE, this.onChartUpdate);
+        chart.removeListener(CHART_EVENTS.DESTROY, this.removeChart);
+    }
+
+    private _bindEvents (chart: Chart): void {
+        chart.addListener(CHART_EVENTS.UPDATE, this.onChartUpdate);
+        chart.addListener(CHART_EVENTS.DESTROY, this.removeChart);
+    }
+
+    public draw() {
+        this.stage.render(this.renderer);
+    }
+}
